58f4e6fa6a9a89267329db4051168c2e4f4aae71
Nikita Babko <niki-babko@yandex.ru>
2020-10-25 20:07:59 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Route, Redirect } from "react-router-dom";
+
+import { Auth, Home } from "pages";
+
+const App = (props) => {
+    const { isAuth } = props;
+    return (
+        <div className="wrapper">
+            <Route exact path={["/login", "/register"]} component={Auth} />
+            <Route
+                exact
+                path="/"
+                render={() =>
+                    console.log(isAuth) ||
+                    (isAuth ? <Home /> : <Redirect to="/login" />)
+                }
+            />
+        </div>
+    );
+};
+
+export default connect(({ user }) => ({ isAuth: user.isAuth }))(App);
---
+++ client/src/modules/LoginForm/containers/LoginForm.js
@@ -0,0 +1,36 @@
+import { withFormik } from "formik";
+
+import LoginForm from "../components/LoginForm";
+
+import validateForm from "utils/validate";
+import { userActions } from "redux/actions";
+
+import store from "redux/store";
+
+const LoginFormContainer = withFormik({
+    enableReinitialize: true,
+    mapPropsToValues: () => ({
+        email: "",
+        password: "",
+    }),
+    validate: (values) => {
+        let errors = {};
+
+        validateForm({ isAuth: true, values, errors });
+
+        return errors;
+    },
+    handleSubmit: (values, { setSubmitting, props }) => {
+        store.dispatch(userActions.fetchUserLogin(values)).then(({ status }) => {
+            if (status === "success") {
+                setTimeout(() => {
+                    props.history.push("/");
+                }, 50);
+            }
+            setSubmitting(false);
+        });
+    },
+    displayName: "LoginForm",
+})(LoginForm);
+
+export default LoginFormContainer;
