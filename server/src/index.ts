d1c96c77905a51b98a4fe8aab844bfee9d4f864b
Nikita Babko <niki-babko@yandex.ru>
2021-03-31 19:54:30 +0700

    Linting.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,12 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { createServer } from 'http';
+import express from "express";
+import dotenv from "dotenv";
+import { createServer } from "http";
 
 dotenv.config();
 
-import './core/db';
-import createRoutes from './core/routes';
-import createSocket from './core/socket';
+import "./core/db";
+import createRoutes from "./core/routes";
+import createSocket from "./core/socket";
 
 const app = express();
 const http = createServer(app);
--- server/src/utils/generatePasswordHash.ts
+++ server/src/utils/generatePasswordHash.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
 export default (password: string): Promise<string> => {
   return new Promise((resolve, reject) => {
